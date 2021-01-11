e7e8e56ed974e6c4ead6ef169d182018cfd3d8a3
irina <irisha.karpova@gmail.com>
2021-01-11 16:33:19 -0600

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../trash/App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme'
