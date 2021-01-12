b2c771d521e63d23fda34bba51e5fd4952e0c7c8
irina <irisha.karpova@gmail.com>
2021-01-11 18:53:30 -0600

    update

---
+++ lib/App.tsx
@@ -0,0 +1,32 @@
+
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import SlideShow from './components/SlideShow'
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme'
+
+interface AppStateValue {
+  serverData: Array <
+    {
+      id: string
+      label: string
+      path: string
+    } 
+  >
+  containerMaxWidth: number
+  containerRatio: number
+  containerShadow: boolean
+  imageShadow: boolean
+  showNextPrev: boolean
+}
+
+
+export default (props: AppStateValue) : JSX.Element => {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <SlideShow {...props}/>
+    </ThemeProvider>
+  );
+}
+
