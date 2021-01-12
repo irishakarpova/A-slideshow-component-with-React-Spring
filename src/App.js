b143dcaf26cee9c80a04f89a20539cd95dd713d5
irina <irisha.karpova@gmail.com>
2021-01-11 20:21:26 -0600

    update

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import SlideShow from '../src/components/SlideShow'
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from '../src/theme'
+
+function App(props) {
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <SlideShow 
+        containerRatio={props.containerRatio}
+        containerMaxWidth={ props.container } 
+        containerShadow={props.containerShadow}
+        imageShadow={props.imageShadow}
+        serverData={props.serverData} 
+        showNextPrev={props.showNextPrev} />
+    </ThemeProvider>
+  );
+}
+export default App;
